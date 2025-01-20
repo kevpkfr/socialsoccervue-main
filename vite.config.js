8576389464165444cd9e41c115e2b9dd345d4334
Kevin <kpinsag@gmail.com>
2025-01-19 22:51:16 -0500

    first commit4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -20,5 +11,4 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  base: '/', // Asegura que las rutas funcionen correctamente en producción
 });
