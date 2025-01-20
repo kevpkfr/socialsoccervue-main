99fcb5cf3cca2ef674b264df65329458363f437f
Kevin <kpinsag@gmail.com>
2025-01-19 23:20:25 -0500

    first commit7

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [
-      { find: '@', replacement: resolve(__dirname, './src') }
-    ],
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
   server: {
-    host: '0.0.0.0', // Permite acceso en Railway
-    port: process.env.PORT || 5173, // Puerto dinámico en Railway
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
     strictPort: true,
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
 });
